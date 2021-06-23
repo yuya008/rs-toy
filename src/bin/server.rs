623071a846550b11131bd3e2bcca8493418b5a11
yuya <yuya008@aliyun.com>
2021-06-23 17:07:34 +0800

    up

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,10 +1,12 @@
 use rs_toy::opt::Opt;
 use rs_toy::server;
+use std::process;
 use structopt::StructOpt;
 
 fn main() {
     let opt = Opt::from_args();
     if let Err(err) = server::run(&opt) {
         eprintln!("{}", err);
+        process::exit(1);
     }
 }
