b2848f460fc7acfd6a9016f326406cec6fd41d91
yuya <yuya008@aliyun.com>
2021-06-25 11:26:20 +0800

    up

--- src/handler/test.rs
+++ src/handler/test.rs
@@ -1,11 +1,20 @@
 use crate::db::DB;
 use actix_web::web::Data;
 use actix_web::{get, HttpRequest, HttpResponse, Result};
+use diesel::Connection;
 
 #[get("/welcome")]
 pub async fn welcome(db: Data<DB>, req: HttpRequest) -> HttpResponse {
     info!("welcome");
     let conn = db.get().expect("couldn't get db connection from pool");
-
-    HttpResponse::Ok().finish()
+    match conn.execute("select * from test") {
+        Err(err) => {
+            warn!("welcome {}", err);
+            HttpResponse::BadRequest().finish()
+        }
+        Ok(n) => {
+            info!("welcome {}", n);
+            HttpResponse::Ok().finish()
+        }
+    }
 }
